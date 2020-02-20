1cf6654bb4380cd4fd4546da46f6f9d7953590bb
nago3579 <nago3579@colorado.edu>
2020-02-20 14:34:07 -0700

    Updates

---
+++ precache-manifest.af97ed403afadff97990928e40fe2e53.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6926df4eca45f8affa",
+    "url": "/hypertextnar/static/js/app.2844bf4f.chunk.js"
+  },
+  {
+    "revision": "84a992015562b767bb06",
+    "url": "/hypertextnar/static/js/runtime~app.f87f939f.js"
+  },
+  {
+    "revision": "595d3d161f206b4e416a",
+    "url": "/hypertextnar/static/js/2.f50f8286.chunk.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/hypertextnar/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/hypertextnar/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/hypertextnar/serve.json"
+  },
+  {
+    "revision": "de896701fdc3a300a2a8e1067c04b4e1",
+    "url": "/hypertextnar/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/hypertextnar/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/hypertextnar/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/hypertextnar/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/hypertextnar/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/hypertextnar/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/hypertextnar/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/hypertextnar/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/hypertextnar/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/hypertextnar/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/hypertextnar/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/hypertextnar/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/hypertextnar/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/hypertextnar/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/hypertextnar/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/hypertextnar/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "e2103d2a1662badfce4d00928fb2ec0a",
+    "url": "/hypertextnar/index.html"
+  },
+  {
+    "revision": "f6d81a13b9a10b211ff376d638a59b39",
+    "url": "/hypertextnar/static/js/app.2844bf4f.chunk.js.gz"
+  },
+  {
+    "revision": "606800bc3f70b1c2a8ffe4d8ab50e9ea",
+    "url": "/hypertextnar/static/js/runtime~app.f87f939f.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/hypertextnar/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "f7bf49919f49bfa60b06396e42abfbeb",
+    "url": "/hypertextnar/static/js/2.f50f8286.chunk.js.gz"
+  }
+];
